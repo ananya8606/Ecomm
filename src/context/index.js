780adb70e97bde6ba3c92d5de6eced0450b080be
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-10 19:43:23 +0530

    Update index.js

--- src/context/index.js
+++ src/context/index.js
@@ -19,7 +19,7 @@ const StoreProvider = ({children}) => {
     const [sortByIndex, setSortByIndex] = useState(0)
     const [viewGrid, setViewGrid] = useState(true)
 
-    const {response: products, error } = useFetch('https://course-api.com/react-store-products')
+    const {response: products, error } = useFetch(' https://api.jsonbin.io/b/618bd230820eda3cc81b001f/1 ')
 
     if(error) return 'error....';
     
