0071e1a19506c7f24657d075afcf492f2258f503
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-11 13:49:27 +0530

    Update home.js

--- src/templates/home.js
+++ src/templates/home.js
@@ -103,7 +103,7 @@ const Home = () => (
             <div className="transform py-20 xl:py-0 xl:translate-y-20 tw-container text-blue-50">
                 <div className="grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 items-center gap-y-4">
                     <h3 className="font-bold">Handmade Bamboo Products<br/>Designed Only For You</h3>
-                    <p className="leading-7">Handmade Bamboo Products</p>
+                    <p className="leading-7">Fall in love with our Bamboo Products</p>
                 </div>
                 <div className="mt-16 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 text-center gap-10">
                     <ServiceBox title="Mission">
