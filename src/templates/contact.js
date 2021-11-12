4444cb8d80b4f47bc0e090ddaeb0e6c1d6ebc0d4
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-12 22:41:52 +0530

    Update contact.js

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Breadcrumbs from '../components/Breadcrumbs'
 import { Link } from 'react-router-dom'
 
