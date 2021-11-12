450858e006edeaa3ac11d11a0e1dbe56df7a1891
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-12 22:46:14 +0530

    Update contact.js

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -113,13 +113,7 @@ const Contact = () => (
         </div>
         <div class="col-12 col-lg-6">
           <h4>Where to find us:</h4>
-          <div class="embed embed-responsive-4by3">
-            <embed
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3602.376113531558!2d78.63823701454498!3d25.45911358377501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39777701fa29d96b%3A0x274c37abc80c1ad1!2sB.I.E.T!5e0!3m2!1sen!2sin!4v1513400712400"
-              width="600"
-              height="450"
-            />
-          </div>
+          
         </div>
       </div>
     </div>
