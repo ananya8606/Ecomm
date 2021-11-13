a81131c574a8216e60eb7e6507cf5605e9688188
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-13 15:07:43 +0530

    Update contact.jsx

--- src/templates/contact.jsx
+++ src/templates/contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import GMap from './GMap';
 import Breadcrumbs from '../components/Breadcrumbs'
 import { Link } from 'react-router-dom'
