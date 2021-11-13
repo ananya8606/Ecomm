4bfcd67a568d34659fab2da593696602769fa322
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-13 13:26:59 +0530

    Update contact.jsx

--- src/templates/contact.jsx
+++ src/templates/contact.jsx
@@ -4,18 +4,46 @@ import { Link } from 'react-router-dom'
 import Form from './Form'
 import Info from './Info'
 import './contact-section.css'
+import React, { useState, useEffect } from 'react';
+import GMap from './GMap';
 
-const Contact = () => (
-  <>
-<Breadcrumbs>
+// API key of the google map
+const GOOGLE_MAP_API_KEY = '<ABQIAAAAvZMU4-DFRYtw1UlTj_zc6hT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQcT1h-VA8wQL5JBdsM5JWeJpukvw>';
+
+// load google map script
+const loadGoogleMapScript = (callback) => {
+  if (typeof window.google === 'object' && typeof window.google.maps === 'object') {
+    callback();
+  } else {
+    const googleMapScript = document.createElement("script");
+    googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAP_API_KEY}`;
+    window.document.body.appendChild(googleMapScript);
+    googleMapScript.addEventListener("load", callback);
+  }
+}
+
+const Contact = () => {
+  const [loadMap, setLoadMap] = useState(false);
+
+  useEffect(() => {
+    loadGoogleMapScript(() => {
+      setLoadMap(true)
+    });
+  }, []);
+
+  return (
+    <Breadcrumbs>
             <Link to="/">Home</Link>
             Contact us
         </Breadcrumbs>
     <div className="contact-section">
       <Form />
       <Info />
+      <a href="https://www.cluemediator.com">Clue Mediator</a><br /><br />
+      {!loadMap ? <div>Loading...</div> : <GMap />}
+      <br/><br/>
+      <small><b>Note:</b> In order to make it work, you have to set the YOUR_GOOGLE_MAP_API_KEY in App.js file. </small>
     </div>
-  </>
-)
+  );
 
-export default Contact
+export default Contact;
