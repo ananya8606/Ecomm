cd7bd7b5ee6f0e1f4b0ed506b990263cd59ac013
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-13 22:02:48 +0530

    Update contact.jsx

--- src/templates/contact.jsx
+++ src/templates/contact.jsx
@@ -14,9 +14,6 @@ const Contact = () => (
 <div className="center">
 <embed
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3602.376113531558!2d78.63823701454498!3d25.45911358377501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39777701fa29d96b%3A0x274c37abc80c1ad1!2sB.I.E.T!5e0!3m2!1sen!2sin!4v1513400712400"
-              width="800"
-              height="250"
-              
             />
 </div>
 <div className="contact-section">
