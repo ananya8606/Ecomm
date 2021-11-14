7022b860fd8371c0412565b4ef8d49990a8314b1
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-14 08:20:06 +0530

    Update Info.jsx

--- src/templates/Info.jsx
+++ src/templates/Info.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Icon, InlineIcon } from '@iconify/react'
-
 import locationIcon from '@iconify/icons-mdi/map-marker-radius-outline'
 import phoneIcon from '@iconify/icons-mdi/phone-outline'
 import emailIcon from '@iconify/icons-mdi/email-multiple-outline'
-
 import facebookIcon from '@iconify/icons-mdi/facebook'
 import linkedinIcon from '@iconify/icons-mdi/linkedin'
 import twitterIcon from '@iconify/icons-mdi/twitter'
-
 import './info.css'
 
 const contactDetails = [
   {
-    value: 'DIC Campus Ratu Road,Ranchi - JH 834001,INDIA.',
+    value: 'Bundu,Ranchi,Jharkhand-835204,INDIA.',
     icon: locationIcon,
   },
   { value: '+91 7654325689', icon: phoneIcon },
