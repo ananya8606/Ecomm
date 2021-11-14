b2aadeaf3312991846bbc08a947f21192be73380
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-14 14:43:16 +0530

    Update checkout.js

--- src/templates/checkout.js
+++ src/templates/checkout.js
@@ -2,13 +2,6 @@ import React, {Component,useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Link, useHistory } from 'react-router-dom'
 import Breadcrumbs from '../components/Breadcrumbs'
-import CheckoutForm from './CheckoutForm'
-import { Container, Col, Row } from "reactstrap"
-import ContactInfo from './ContactInfo'
-import ShippingMethod from './ShippingMethod'
-import Payment from './Payment';
-import Billing from './Billing';
-import Cart from './cart'	
 export default function Checkout() {
     const { isAuthenticated } = useAuth0();
     const history = useHistory()
@@ -24,19 +17,6 @@ export default function Checkout() {
                 <Link to="/">Home</Link>
                 Checkout
             </Breadcrumbs>
-<Container>
-                <Row>
-                    <Col className="left-col-container" md="6">
-                        <ContactInfo />
-                        <ShippingMethod />
-                        <Payment />
-                        <Billing />
-                    </Col>
-                    <Col className="right-col-container pb-4" md="6">
-                        <Cart />
-                    </Col>
-                </Row>
-            </Container>
 </>
 		);
 	}
