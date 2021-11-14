9d799ef3240bafb0eff329a39aa27b3e471fd3d2
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-14 12:13:57 +0530

    Update checkout.js

--- src/templates/checkout.js
+++ src/templates/checkout.js
@@ -1,37 +1,18 @@
-import React,{useEffect} from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { Link, useHistory } from 'react-router-dom';
-import Breadcrumbs from '../components/Breadcrumbs';
-import { Container, Col, Row } from "reactstrap";
-import ContactInfo from './form_components/ContactInfo';
-import ShippingMethod from './form_components/ShippingMethod';
-import Payment from './form_components/Payment';
-import Billing from './form_components/Billing';
-import Cart from './cart';
+import React, { useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Link, useHistory } from 'react-router-dom'
+import Breadcrumbs from '../components/Breadcrumbs'
 
-class Checkout extends Component {
-const { isAuthenticated } = useAuth0();
+
+export default function Checkout() {
+    const { isAuthenticated } = useAuth0();
     const history = useHistory()
 
     useEffect(() => {
         if(!isAuthenticated) history.push('/')
     }, [isAuthenticated, history]);
 
-	constructor(props) {
-        super(props);
-        
-        this.state = {
-            data : ""
-		};
-    
-    }
-
-    formChild1(params) {
-        this.setState({
-          data : params
-        })
-      }
-if(isAuthenticated) {
+    if(isAuthenticated) {
         return (
             <Breadcrumbs>
                 <Link to="/">Home</Link>
@@ -39,22 +20,6 @@ if(isAuthenticated) {
             </Breadcrumbs>
         )
     }
-render() {
-        return (
-            <Container>
-                <Row>
-                    <Col className="left-col-container" md="6">
-                        <ContactInfo />
-                        <ShippingMethod callback={this.formChild1.bind(this)} />
-                        <Payment />
-                        <Billing />
-                    </Col>
-                    <Col className="right-col-container pb-4" md="6">
-                        <Cart />
-                    </Col>
-                </Row>
-            </Container>
-		);
-	}
+
+    return null;
 }
-export default Checkout;
