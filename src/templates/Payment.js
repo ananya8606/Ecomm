763eb47ca263203bd07f55aa1f7130e1114d9e73
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-14 14:01:44 +0530

    Update Payment.js

--- src/templates/Payment.js
+++ src/templates/Payment.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Button, Form, FormGroup, Input } from "reactstrap";
 import CreditCardInput from 'react-credit-card-input';
 
