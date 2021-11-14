0147d0f945e29371d2db6fbf0045eea306154f41
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2021-11-14 11:15:22 +0530

    Update checkout.js

--- src/templates/checkout.js
+++ src/templates/checkout.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Component} from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Link, useHistory } from 'react-router-dom'
 import Breadcrumbs from '../components/Breadcrumbs'
-import { Component } from 'react';
 import { Container, Col, Row } from "reactstrap";
 import ContactInfo from './form_components/ContactInfo';
 import ShippingMethod from './form_components/ShippingMethod';
