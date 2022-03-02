e5c4fd6c8f739b0f0201be5a1b477ff1caedf377
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2022-03-02 20:19:08 +0530

    Update checkout.js

--- src/templates/checkout.js
+++ src/templates/checkout.js
@@ -1,6 +1,7 @@
 import React, {useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import {useHistory } from 'react-router-dom'
+import {Link,useHistory } from 'react-router-dom'
+import Breadcrumbs from '../components/Breadcrumbs'
 export default function Checkout() {
     const { isAuthenticated } = useAuth0();
     const history = useHistory()
