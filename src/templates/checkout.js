c3b0df324564e098afcd43d2da4a74bb93cde847
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2022-03-02 21:56:22 +0530

    Update checkout.js

--- src/templates/checkout.js
+++ src/templates/checkout.js
@@ -72,7 +72,7 @@ export default function Checkout() {
         </div>
         <div class="col-md-7 col-lg-8">
             <h4 class="mb-3">Billing address</h4>
-            <form class="needs-validation" novalidate>
+            <form action="/tutorial/action.html" class="needs-validation" novalidate>
                 <div class="row g-3">
                     <div class="col-sm-6">
                         <label for="firstName" class="form-label">First name</label>
