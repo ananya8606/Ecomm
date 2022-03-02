191e795396745960bdb7cb4578f3c9e2f2b806a7
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2022-03-02 18:46:31 +0530

    Update checkout.js

--- src/templates/checkout.js
+++ src/templates/checkout.js
@@ -2,19 +2,20 @@ import React, {useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { Link, useHistory } from 'react-router-dom'
 import Breadcrumbs from '../components/Breadcrumbs'
+import Payment from './payment'
 export default function Checkout() {
     const { isAuthenticated } = useAuth0();
     const history = useHistory()
 
     useEffect(() => {
-        if(!isAuthenticated) history.push('/')
+        if(!isAuthenticated) history.push('/payment')
     }, [isAuthenticated, history]);
 
     if(isAuthenticated) {
         return (
 <>
             <Breadcrumbs>
-                <Link to="/">Home</Link>
+                <Link to="/payment">Home</Link>
                 Checkout
             </Breadcrumbs>
 </>
