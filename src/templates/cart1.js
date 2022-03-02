90a77364e961129e199852bbb15bd00502f14db0
ananya8606 <52853286+ananya8606@users.noreply.github.com>
2022-03-03 02:41:20 +0530

    Update cart1.js

--- src/templates/cart1.js
+++ src/templates/cart1.js
@@ -69,12 +69,14 @@ export default function Cart1() {
                                         <div class="col-sm-4">
                                         <h5 className="hidden md:block">{(price*20).toCurrency()}</h5>
                                         </div>
+                                        <div class="col-sm-4">
                                         <Counter
                                             className="text-1xl md:text-2xl"
                                             count={amount}
                                             setCount={(value) => handleAmountChange(value, max, i)}
                                             maxNum={max}
                                         />
+                                        </div>
                                         <button className="block ml-3 md:ml-0 w-7 h-7 p-2 bg-red-600 text-white rounded" onClick={() => handleDeleteItem(i)}>
                                             <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512"><path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"></path></svg>
                                         </button>
